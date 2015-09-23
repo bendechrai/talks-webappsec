e9ec042f6ae7e1641884a4f40b3ef82f5a3c7eb8
Ben Dechrai <ben@dechrai.com>
2015-09-23 12:17:54 +0800

    Set apache to run as vagrant user

--- evil.com/bootstrap.sh
+++ evil.com/bootstrap.sh
@@ -8,6 +8,11 @@ echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-
 echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
 aptitude install -y mysql-client mysql-server libapache2-mod-php5 php5{,-{cli,gd,json,mcrypt,mysql,dev}} php-pear
 
+# MAKE APACHE RUN AS VAGRANT
+sed -i 's@www-data@vagrant@' /etc/apache2/envvars 
+chown vagrant /var/lock/apache2
+service apache2 restart
+
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant/www /var/www
--- good.com/bootstrap.sh
+++ good.com/bootstrap.sh
@@ -8,6 +8,11 @@ echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-
 echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
 aptitude install -y mysql-client mysql-server libapache2-mod-php5 php5{,-{cli,gd,json,mcrypt,mysql,dev}} php-pear
 
+# MAKE APACHE RUN AS VAGRANT
+sed -i 's@www-data@vagrant@' /etc/apache2/envvars 
+chown vagrant /var/lock/apache2
+service apache2 restart
+
 mysql -uroot -p$DBPASSWD -e 'create database understanding';
 php ./db_create.php
 
